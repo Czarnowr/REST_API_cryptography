381a7487aa08b16787ef7b6d060ab9596284f854
Radek <czarnowr@gmail.com>
2019-05-27 13:26:55 +0200

    first commit

---
+++ src/main/java/net/usermd/czarnowr/cryptography/CryptographyApplication.java
@@ -0,0 +1,13 @@
+package net.usermd.czarnowr.cryptography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptographyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptographyApplication.class, args);
+    }
+
+}
