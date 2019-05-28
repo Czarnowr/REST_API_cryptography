a54331e1e10c467eb67c6cea69ee18b120ca6885
Radek <czarnowr@gmail.com>
2019-05-28 22:11:28 +0200

    Thymeleaf config

---
+++ src/main/java/net/usermd/czarnowr/cryptography/configuration/WebConfig.java
@@ -0,0 +1,22 @@
+package net.usermd.czarnowr.cryptography.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableAsync
+public class WebConfig implements WebMvcConfigurer {
+
+    public WebConfig() {
+        super();
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+}
